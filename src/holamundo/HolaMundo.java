85b266dd8104ee9eaee7ea579cbec858e735ef64
Pontifice Shullyvan <rframosduran7@gmail.com>
2019-04-01 12:01:26 +0200

    Codigo fuente inicial

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,19 @@
+package holamundo;
+
+/**
+ *
+ * @author rafa
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hola mundo");
+        System.out.println("Ejemplo para pruebas");
+
+    }
+    
+}
